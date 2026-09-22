d8fe02cee242de791fe2f97020820ef2e02915c1
lupolugo <ascomitll@gmail.com>
2026-09-22 23:04:37 +0200

    LL

--- js/main.js
+++ js/main.js
@@ -128,7 +128,7 @@ async function renderBooksGrid(targetSelector) {
     target.innerHTML = books
       .map(
         (book) => `
-        <div class="book-spine">${book.year}</div> 
+        <div class="book-year">${book.year}</div> 
         <a class="book-item" href="libro.html?id=${book.id}">
           <div class="book-spine">        
             <div class="book-cover">
